e6db7d92f2bf93b234e23ddbf5c09e6904542781
shakilla1 <shakilshifat65@gmail.com>
2022-10-12 13:59:11 +0600

    addd question 2

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -6,6 +6,9 @@ const Blog = () => {
             <h1 className='text-3xl font-bold pl-5 pt-5'>Whare is The purpose of react router ?</h1>
             <p className='text-2xl font-xl pl-5 pt-2'><span className='font-semibold'>Ans:</span>React Router is a standard library for routing in React.</p>
 
+            <h1 className='text-3xl font-bold pl-5 pt-5'>How dose Contex API works?</h1>
+            <p className='text-2xl font-xl pl-5 pt-2'><span className='font-semibold'>Ans:</span> The React Context API is a way for a React app to effectively produce global variables that can be passed around. </p>
+
 
         </div>
 
