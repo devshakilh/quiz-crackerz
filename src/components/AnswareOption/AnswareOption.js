0a4547b7903e9c7d75f03c8a9ae87d1b2c45a882
shakilla1 <shakilshifat65@gmail.com>
2022-10-12 20:18:49 +0600

    complete website

--- src/components/AnswareOption/AnswareOption.js
+++ src/components/AnswareOption/AnswareOption.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
 const AnswareOption = ({ quiz }) => {
-    function myfun() {
-        toast("Right Answare!");
+    const { correctAnswer } = quiz;
+
+    function myfun(e) {
+        const quixs = (e.target.value)
+        if (quiz === correctAnswer) {
+            toast.success('right')
+        }
+
     }
+
 
     return (
         <div className=''>
             <ul className=' bg-teal-200 border-solid border-2 border-sky-500 mx-4 my-2 py-3 px-3 font-medium'>
 
-                <li><input onClick={myfun} type="radio" name="" id="" />{quiz}</li>
+                <li><input onClick={(e) => myfun(e)} type="radio" name="" id="" />{quiz}</li>
 
             </ul>
         </div >
